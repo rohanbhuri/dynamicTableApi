75286f145c90f6564c09b262e6c8f0d4528f0801
rohanbhuri123 <rohan@amazecreators.com>
2018-01-10 18:10:49 +0530

    small chnages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatIconModule, MatInputModule, MatSelectModule } from '@angular/material';
+import { MatIconModule, MatInputModule, MatSelectModule, MatButtonModule } from '@angular/material';
+import { AgmCoreModule } from '@agm/core';
+import { AgmSnazzyInfoWindowModule } from '@agm/snazzy-info-window';
+
+
 
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 const appRoutes: Routes = [
@@ -20,6 +25,7 @@ const appRoutes: Routes = [
     AppComponent,
     HeaderComponent,
     HomeComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
@@ -28,9 +34,13 @@ const appRoutes: Routes = [
     MatIconModule,
     MatInputModule,
     MatSelectModule,
+    MatButtonModule,
 
     RouterModule.forRoot(appRoutes),
-
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyB8xLGb6Yx0ZOrHAYCsSd8L3E92g8xW1bM'
+    }),
+    AgmSnazzyInfoWindowModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,13 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   slideNumber = 0;
-  constructor() { }
+  lat: Number = 51.678418;
+  lng: Number = 7.809007;
+  constructor(
+    public ngZone: NgZone,
+  ) {
+    this.sliderAuto();
+  }
 
   ngOnInit() {
+
+  }
+
+  sliderAuto() {
+    setInterval(() => {
+      this.ngZone.run(() => {
+        if (this.slideNumber === 2) { this.slideNumber = 0; return; }
+        if (this.slideNumber === 0) { this.slideNumber = 1; return; }
+        if (this.slideNumber === 1) { this.slideNumber = 2; return; }
+        console.log('auto', this.slideNumber);
+      });
+    }, 4000);
   }
 
   scrollDown() {
-    document.getElementById('body').scrollTop = 100;
+    document.querySelector('.section-2').scrollIntoView({
+      behavior: 'smooth'
+    });
   }
 
 }
