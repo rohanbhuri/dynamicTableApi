56e5d6679470e135154290cbecc58d1fae049894
rohanbhuri123 <rohan@amazecreators.com>
2018-02-07 11:32:46 +0530

    small chnages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HttpModule } from '@angular/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AgmCoreModule } from '@agm/core';
-import { AgmSnazzyInfoWindowModule } from '@agm/snazzy-info-window';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {
   MatIconModule,
@@ -26,45 +24,18 @@ import { RestaurantService } from './services/restaurant.service';
 
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { HomeComponent } from './components/home/home.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { SignupComponent } from './components/signup/signup.component';
-import { SigninComponent } from './components/signin/signin.component';
-import { RouterTransition } from './router.animations';
-
-
-import { ContactUsComponent } from './components/contact-us/contact-us.component';
-import { FaqComponent } from './components/faq/faq.component';
-import { ProfileComponent } from './components/profile/profile.component';
-import { LogoutComponent } from './components/logout/logout.component';
 
 
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: 'profile', component: ProfileComponent },
-  { path: 'signup', component: SignupComponent, canActivate: [AuthGuardService] },
-  { path: 'signin', component: SigninComponent, canActivate: [AuthGuardService] },
-  { path: 'contact-us', component: ContactUsComponent },
-  { path: 'faq', component: FaqComponent },
-  { path: 'logout', component: LogoutComponent }
+  // { path: 'logout', component: LogoutComponent }
 
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    HomeComponent,
-    FooterComponent,
-    SignupComponent,
-    SigninComponent,
-    ContactUsComponent,
-    FaqComponent,
-    ProfileComponent,
-    LogoutComponent,
   ],
   imports: [
     BrowserModule,
@@ -84,10 +55,6 @@ const appRoutes: Routes = [
     MatProgressSpinnerModule,
 
     RouterModule.forRoot(appRoutes),
-    AgmCoreModule.forRoot({
-      apiKey: 'AIzaSyB8xLGb6Yx0ZOrHAYCsSd8L3E92g8xW1bM'
-    }),
-    AgmSnazzyInfoWindowModule
   ],
   providers: [AuthService, AuthGuardService, RestaurantService],
   bootstrap: [AppComponent]
