70177f9e757fc2ab5830379e60061968a30a1bad
rohanbhuri123 <rohan@amazecreators.com>
2018-02-20 17:31:14 +0530

    small chnages

--- src/app/components/open-table/open-table.component.ts
+++ src/app/components/open-table/open-table.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { TableService } from '../../services/table.service';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+import { MatSnackBar } from '@angular/material';
 
 
 @Component({
@@ -18,7 +19,8 @@ export class OpenTableComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private location: Location,
-    private tableService: TableService
+    private tableService: TableService,
+    public snackBar: MatSnackBar
   ) {
     this.setValues();
   }
@@ -41,4 +43,69 @@ export class OpenTableComponent implements OnInit {
       }
     });
   }
+
+  applyFilter(filterValue: string) {
+    this.data._schema = [];
+    let array = [];
+    filterValue = filterValue.trim(); // Remove whitespace
+    if (filterValue === '') {
+      this.setValues();
+    } else {
+      const data = {
+        id: this.id
+      };
+      this.tableService.getTable(data).subscribe(res => {
+        array = res.table._schema;
+        array.forEach((element, key) => {
+          console.log(element.fieldName === filterValue);
+          if (element.fieldName === filterValue) {
+            this.data._schema.push(element);
+          } else {
+            this.data._schema.splice(key, 1);
+          }
+        });
+      });
+    }
+  }
+
+  downloadTable() {
+    // const params = {
+    //   search: this.dataSource.filter,
+    //   sort: { [this.sort.active]: this.sort.direction === 'desc' ? -1 : 1 }
+    // };
+    // this.tableService.downloadTableList(params).subscribe(res => {
+    //   console.log(res);
+    //   const parsedResponse = res.result;
+    //   const blob = new Blob([parsedResponse], { type: 'text/csv' });
+    //   const url = window.URL.createObjectURL(blob);
+    //   if (navigator.msSaveOrOpenBlob) {
+    //     navigator.msSaveBlob(blob, 'TableList.csv');
+    //   } else {
+    //     const a = document.createElement('a');
+    //     a.href = url;
+    //     a.download = 'TableList.csv';
+    //     document.body.appendChild(a);
+    //     a.click();
+    //     document.body.removeChild(a);
+    //   }
+    //   window.URL.revokeObjectURL(url);
+    // });
+  }
+
+  uploadTable(event) {
+    const reader = new FileReader();
+    reader.onload = (e: any) => {
+      const base64Data = e.target.result.split('base64,');
+      console.log(base64Data[1]);
+      this.tableService.uploadTable({ csv: atob(base64Data[1]) }).subscribe(res => {
+        console.log(res);
+        this.snackBar.open(res.message, 'OK', {
+          duration: 3000,
+        });
+        // this.getData();
+      });
+    };
+    reader.readAsDataURL(event.target.files[0]);
+  }
 }
+
