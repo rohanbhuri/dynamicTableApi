42a5181983d83196484172aa3657e4227ddaa9a1
rohanbhuri123 <rohan@amazecreators.com>
2018-03-06 11:23:08 +0530

    small changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://dynamic-table-api.herokuapp.com/api/',
-  siteUrl: 'https://dynamic-table-api.herokuapp.com/',
+  apiUrl: 'https://dynamictableapi.appspot.com/api/',
+  siteUrl: 'https://dynamictableapi.appspot.com/',
 };
