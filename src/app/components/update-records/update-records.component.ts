9753896f988d771604e7056a314f646c6c7acd8d
rohanbhuri123 <rohan@amazecreators.com>
2018-03-07 13:21:18 +0530

    changes done

--- src/app/components/update-records/update-records.component.ts
+++ src/app/components/update-records/update-records.component.ts
@@ -42,7 +42,7 @@ export class UpdateRecordsComponent implements AfterViewInit {
   constructor(
     public ngZone: NgZone,
     private route: ActivatedRoute,
-    private location: Location,
+    public location: Location,
     private tableService: TableService,
     public snackBar: MatSnackBar
   ) {
